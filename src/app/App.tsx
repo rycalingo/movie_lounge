e6538b032a020cf01a0aac25c7ea5c395cb0453c
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-21 18:14:05 -0400

    Add a react router to handle page routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-
-import './App.css';
+import { AllRoutes } from "../routes/AllRoutes";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className="App">
+      <AllRoutes />
+    </div>
+  );
 }
 
 export default App;
