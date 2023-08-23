c1acd2cf30150717a33c3c915dda15148899e63a
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-23 18:45:00 -0400

    Adjust footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="bg-white shadow m-4 dark:bg-gray-800">
+    <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2030{" "}
@@ -13,29 +13,17 @@ export const Footer = () => {
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <Link
-              to="#"
-              target="_blank"
-              className="mr-4 hover:underline md:mr-6 "
-            >
+            <Link to="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">
               About
             </Link>
           </li>
           <li>
-            <Link
-              to="#"
-              target="_blank"
-              className="mr-4 hover:underline md:mr-6"
-            >
+            <Link to="#" target="_blank" className="mr-4 hover:underline md:mr-6">
               Privacy Policy
             </Link>
           </li>
           <li>
-            <Link
-              to="#"
-              target="_blank"
-              className="mr-4 hover:underline md:mr-6"
-            >
+            <Link to="#" target="_blank" className="mr-4 hover:underline md:mr-6">
               Licensing
             </Link>
           </li>
