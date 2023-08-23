ca5aa3e17a0d8cb7946b0328161dce26c46595da
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-23 17:24:41 -0400

    Change nav bar to use mapping for page links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,34 @@
 import { Link, NavLink } from "react-router-dom";
 import logo from "../static/logo.svg";
 
+import { pages } from "../content/pages";
+import { Page } from "../models/page";
+
 export const Header = () => {
+  const activeNav =
+    "text-base block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 active";
+  const inactiveNav =
+    "text-base block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+
+  const navBar = pages?.map((item: Page, i: number) => {
+    return (
+      <li key={i}>
+        <NavLink
+          to={item.path}
+          className={({ isActive }) => (isActive ? activeNav : inactiveNav)}
+          end
+        >
+          {item.title}
+        </NavLink>
+      </li>
+    );
+  });
+
   return (
     <header>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link to="https://flowbite.com/" className="flex items-center">
+          <Link to="/" className="flex items-center">
             <img src={logo} className="h-8 mr-3" alt="Movie Lounge Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Movie Lounge
@@ -118,39 +140,7 @@ export const Header = () => {
               />
             </div>
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <NavLink
-                  to="/"
-                  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                  end
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/movies/popular"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Popular
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/movies/top"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Top Rated
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/movies/upcoming"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Upcoming
-                </NavLink>
-              </li>
+              {navBar}
             </ul>
           </div>
         </div>
---
+++ src/content/pages.ts
@@ -0,0 +1,18 @@
+export const pages = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Popular",
+    path: "/movies/popular",
+  },
+  {
+    title: "Top Rated",
+    path: "/movies/top",
+  },
+  {
+    title: "Upcoming",
+    path: "/movies/upcoming",
+  },
+];
