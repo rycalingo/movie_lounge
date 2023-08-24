23c64974e38c4095c0df11ef431f0b15a17a2025
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-24 14:45:08 -0400

    Add api request to get movie data from TMDB

--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import { Card } from "../components";
 
+const api_url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}`;
+
 export const MovieList = () => {
-  const movieList = [{}];
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  async function fetchMovies() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log(data);
+    setMovies(data.results);
+  }
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-star flex-wrap">
-          {movieList.map((movie, i) => (
+          {movies.map((movie, i) => (
             <Card key={i} />
           ))}
         </div>
