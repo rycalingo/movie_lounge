2c6f8d8fe23f5172a5feb9e653fe69bb709672e7
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-25 13:16:56 -0400

    Add a prop to movieList for the different routes

--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -2,20 +2,26 @@ import { Card } from "../components";
 import { Movie } from "../models/movie";
 import { useFetch } from "../hooks/useFetch";
 
-const api_url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}`;
+interface MovieListProps {
+	api?: string | null;
+}
 
-export const MovieList = () => {
-  const { data: movies } = useFetch(api_url);
+// Url api
+// `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}`;
 
-  return (
-    <main>
-      <section className="max-w-7xl mx-auto py-7">
-        <div className="flex justify-center flex-wrap other:justify-evenly">
-          {movies?.map((movie: Movie, i) => (
-            <Card key={movie?.id ? movie?.id : i} movie={movie} />
-          ))}
-        </div>
-      </section>
-    </main>
-  );
+export const MovieList = ({ api = "" }: MovieListProps) => {
+	const url = `https://api.themoviedb.org/3/${api}?api_key=${process.env.REACT_APP_API_KEY}`;
+	const { data: movies } = useFetch(url);
+
+	return (
+		<main>
+			<section className="max-w-7xl mx-auto py-7">
+				<div className="flex justify-center flex-wrap other:justify-evenly">
+					{movies?.map((movie: Movie, i) => (
+						<Card key={movie?.id ? movie?.id : i} movie={movie} />
+					))}
+				</div>
+			</section>
+		</main>
+	);
 };
--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -2,17 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import { MovieList, MovieDetail, Search, PageNotFound } from "../pages";
 
 export const AllRoutes = () => {
-  return (
-    <div className="dark:bg-gray-900">
-      <Routes>
-        <Route path="" element={<MovieList />} />
-        <Route path="movie/:id" element={<MovieDetail />} />
-        <Route path="movies/popular" element={<MovieList />} />
-        <Route path="movies/top" element={<MovieList />} />
-        <Route path="movies/upcoming" element={<MovieList />} />
-        <Route path="search" element={<Search />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="dark:bg-gray-900">
+			<Routes>
+				<Route path="" element={<MovieList api="movie/now_playing" />} />
+				<Route path="movie/:id" element={<MovieDetail />} />
+				<Route path="movies/popular" element={<MovieList api="movie/popular" />} />
+				<Route path="movies/top" element={<MovieList api="movie/top_rated" />} />
+				<Route path="movies/upcoming" element={<MovieList api="movie/upcoming" />} />
+				<Route path="search" element={<Search />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</div>
+	);
 };
