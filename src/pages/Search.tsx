f5a0ceeb4a87a75807c414dc415a96196d206311
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-26 14:07:19 -0400

    Add an api url for search terms

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+import { Card } from "../components";
+import { Movie } from "../models/movie";
+import { useSearchParams } from "react-router-dom";
 
-export const Search = () => {
-  return <div>Search</div>;
+import { useFetch } from "../hooks/useFetch";
+
+const key = process.env.REACT_APP_API_KEY || "";
+
+export const Search = ({ apiPath = "" }) => {
+	const [searchParams] = useSearchParams();
+	const searchTerm = searchParams.get("q");
+	const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${key}&query=${searchTerm}`;
+
+	const { data } = useFetch(url);
+	console.log(data);
+	const movies = data?.total_results ? data.results : null;
+
+	return (
+		<main>
+			<section>
+				<p className="text-2xl text-gray-700 dark:text-white">
+					{movies !== null ? data?.total_results : `No`} results found '{searchTerm}'
+				</p>
+			</section>
+			<section className="max-w-7xl mx-auto py-7">
+				<div
+					className="flex justify-start flex-wrap
+          other:justify-evenly"
+				>
+					{movies !== null ? movies?.map((movie: Movie, i: number) => <Card key={movie?.id ? movie?.id : i} movie={movie} />) : null}
+				</div>
+			</section>
+		</main>
+	);
 };
