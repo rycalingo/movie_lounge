1abbed78106a5d1a6b36f19fdfeba2f914ff1c8a
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-30 15:41:17 -0400

    Make page scroll to the top when viewing movie details

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import { ScrollToTop } from "./components/ScrollToTop";
+
 import "./index.css";
 import App from "./app/App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Router>
+			<ScrollToTop />
+			<App />
+		</Router>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
