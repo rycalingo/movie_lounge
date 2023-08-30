17261025f151ae1f70edf834285295ee700619a0
Ryan Calingo <gryan.calingo@gmail.com>
2023-08-30 16:29:39 -0400

    Fix error when displaying movie details

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -12,9 +12,9 @@ const key = process.env.REACT_APP_API_KEY || "";
 export const MovieDetail = () => {
 	const param = useParams();
 	const url = `https://api.themoviedb.org/3/movie/${param.id}?api_key=${key}`;
-	const { data: movie } = useFetch(url);
+	const { data } = useFetch(url);
 
-	// const movie = data ? data : {};
+	const movie = data ? data : {};
 
 	const poster_size = "w500/";
 	const img_path = `https://image.tmdb.org/t/p/${poster_size}`;
@@ -36,8 +36,11 @@ export const MovieDetail = () => {
 				<div className="max-w-2xl text-grey-700 text-lg p-4 dark:text-white">
 					<h1 className="text-4xl font-bold my-3">{movie?.title}</h1>
 
-					<p className="tagline italic">"{movie?.tagline}"</p>
-					<p className="my-4">{movie?.overview}</p>
+					<p className="tagline italic">{movie?.tagline?.length ? `"${movie?.tagline}"` : " "}</p>
+					<div className="overview">
+						<h2 className="text-2xl my-4 mb-2">Overview</h2>
+						<p className="">{movie?.overview}</p>
+					</div>
 					<div className="release-date runtime"></div>
 					{movie?.genres ? (
 						<p className="my-7 flex flex-wrap gap-2">
@@ -59,14 +62,14 @@ export const MovieDetail = () => {
 								<title>Rating star</title>
 								<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
 							</svg>
-							<p className="ml-2 text-gray-900 dark:text-white">{movie?.vote_average}</p>
+							<p className="ml-2 text-gray-900 dark:text-white">{movie?.vote_average ? movie?.vote_average : 0}</p>
 							<span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-							<span className="text-gray-900 dark:text-white">{movie?.vote_count} reviews</span>
+							<span className="text-gray-900 dark:text-white">{movie?.vote_count ? movie?.vote_count : 0} reviews</span>
 						</div>
 
 						<div className="status">
 							<span className="font-bold">Status: </span>
-							{movie?.status} {movie?.release_date} ({movie?.production_countries[0].iso_3166_1})
+							{movie?.status} {movie?.release_date} ({movie?.production_countries?.length ? `${movie?.production_countries[0]?.iso_3166_1}` : "–"})
 						</div>
 						<div className="runtime">
 							<p className="">
